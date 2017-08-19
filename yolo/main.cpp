5d3fb7b98a4f123bd56a4bec0c8901c2acfdef7e
ion <ionix@protonmail.com>
2017-08-19 18:18:22 +0200

    fixes

--- yolo/main.cpp
+++ yolo/main.cpp
@@ -14,28 +14,29 @@
 const int xres = 1280;
 const int yres = 720;
 
-char *fragmentSource = 
-"#version 330 core"
-"uniform float time;"
-"uniform vec2 resolution;"
-"void main()"
-"{"
-    "vec2 r = resolution,"
-    "o = gl_FragCoord.xy - r/2.;"
-    "o = vec2(max(abs(o.x)*.866 + o.y*.5, -o.y)*2. / r.y - .3, atan(o.y,o.x));"    
-    "vec4 s = .07*cos(1.5*vec4(0,1,2,3) + time + o.y + time),"
-    "e = s.yzwx, "
-    "f = max(o.x-s,e-o.x);"
-    "gl_FragColor = dot(clamp(f*r.y,0.,1.), 72.*(s-e)) * (s-.1) + f;"
-	"}";
+const char *fragmentSource =
+"#version 330 core\n"
+"uniform float time;\n"
+"uniform vec2 resolution;\n"
+"out vec4 outColor;\n"
+"void main()\n"
+"{\n"
+"vec2 r = resolution,\n"
+"o = gl_FragCoord.xy - r/2.;\n"
+"o = vec2(max(abs(o.x)*.866 + o.y*.5, -o.y)*2. / r.y - .3, atan(o.y,o.x));\n"
+"vec4 s = .07*cos(1.5*vec4(0,1,2,3) + time + o.y + time),\n"
+"e = s.yzwx,\n"
+"f = max(o.x-s,e-o.x);\n"
+"outColor = dot(clamp(f*r.y,0.,1.), 72.*(s-e)) * (s-.1) + f;\n"
+"}\n";
 
-const GLchar* vertexSource =
-"#version 330 core"
-"in vec2 position;"
-"void main()"
-"{"
-"gl_Position = vec4(position, 0.0, 1.0);"
-"}";
+const char *vertexSource = 
+"#version 330 core\n"
+"in vec2 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position, 0.0, 1.0);\n"
+"}\n";
 
 static void error_callback(int error, const char* description)
 {
@@ -227,8 +228,8 @@ int main()
 						glBindFragDataLocation(shaderProgram, 0, "outColor");
 						glLinkProgram(shaderProgram);
 						glUseProgram(shaderProgram);
-#endif
 					}
+#endif
 				}
 				ImGui::EndMenu();
 			}
